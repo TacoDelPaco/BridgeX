79d857274f014d6add53e9ee94c58ef38d0fe1bc
omghax <josh.salles@gmail.com>
2023-09-28 19:20:36 -0700

    Updating `manifest.json` to adhere to standards Ran `prettier` on `server-worker.js` Adjusted `@xyo-network/logger` version in `package.json` from 0.57.0 to 0.29.0 Added `node-pre-gyp` to `package.json` Removed `*.node` from `.gitignore`

--- bridge-client/service-worker.js
+++ bridge-client/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.37eb8712a6d54c627a4661186d9c23f7.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js",
 );
+
+importScripts("/precache-manifest.37eb8712a6d54c627a4661186d9c23f7.js");
 
 workbox.clientsClaim();
 
@@ -29,6 +29,5 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
 });
